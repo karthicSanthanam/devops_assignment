deb4732d2dfb73b3777cc2789f88ae842e5e8963
KarthicSanthanam <karthic.santhanam@gmail.com>
2022-01-04 15:29:15 +0530

    Test.java added.

---
+++ Bubble3/Test.java
@@ -0,0 +1,22 @@
+package com.java11.scjp.chapter1.welcome2java;
+
+public class Test {
+
+	public static void main(String[] args) {
+		//Integer a = new Integer(10);
+		//Integer b = new Integer(10);
+		
+		int a = 10;
+		int b = 10;
+		System.out.println(a == b);
+		
+		String str1 = "One";
+		String str2 = new String("One");
+		
+		System.out.println(str1 == str2);
+		
+		StringBuffer sb = new StringBuffer();
+		
+	}
+
+}
