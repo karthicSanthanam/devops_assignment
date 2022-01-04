48beda0cc752b7241220ea059e98eaba80189e29
KarthicSanthanam <karthic.santhanam@gmail.com>
2022-01-04 15:38:23 +0530

    Feature1 code change.

--- Bubble3/Test.java
+++ Bubble3/Test.java
@@ -3,19 +3,16 @@ package com.java11.scjp.chapter1.welcome2java;
 public class Test {
 
 	public static void main(String[] args) {
-		//Integer a = new Integer(10);
-		//Integer b = new Integer(10);
 		
-		int a = 10;
-		int b = 10;
+		// Feature1 Changes
+		int a = 100;
+		int b = 100;
 		System.out.println(a == b);
 		
-		String str1 = "One";
-		String str2 = new String("One");
+		String str1 = "Feature1";
+		String str2 = new String("Feature1");
 		
 		System.out.println(str1 == str2);
-		
-		StringBuffer sb = new StringBuffer();
 		
 	}
 
